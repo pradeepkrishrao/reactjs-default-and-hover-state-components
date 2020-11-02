5a8df0f7ef2969413b223ccd679ab7e2a6cb76f8
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-02 10:51:14 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import StateList from "./StateList";
+
+var destination = document.querySelector("#container");
+//UI rendered
+ReactDOM.render(<div><StateList/></div>, destination);
